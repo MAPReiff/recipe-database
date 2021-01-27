49758f2aba735ef93200bb58387283d37dec01b7
Mitchell Reiff <mapr323@gmail.com>
2021-01-26 22:46:56 -0500

    Added time of day
    Added option to select breakfast, lunch, or dinner.

--- recipeGen.php
+++ recipeGen.php
@@ -1,7 +1,6 @@
 <?php
   // vars
   $recipeName           =   $_POST['recipeName'];                         // Fried Chicken
-  $recipeFileName       =   str_replace(' ', '', ucwords($recipeName));   // FriedChicken
   $recipeImage          =   './assets/img' . $_POST['recipeImage'];       // http://link.website/chicken.png
   $recipeServings       =   $_POST['recipeServings'];                     // 12
   $recipePrepTime       =   $_POST['recipePrepTime'];                     // 1h 20m
@@ -9,6 +8,20 @@
   $recipeIngredients    =   $_POST['recipeIngredients'];                  // Chicken, Buttermilk
   $recipeInstructions   =   $_POST['recipeInstructions'];                 // Put chicken in stuff
   $recipeEquipment      =   $_POST['recipeEquipment'];                    // Cast iron
+  $recipeTimeOfDay      =   $_POST['recipeTimeOfDay'];                    // Lunch
+
+  if($recipeTimeOfDay=="Breakfast"){
+    $recipeFileName     =   str_replace(' ', '', ucwords($recipeName));
+    $recipeFile         =   './breakfast/' . $recipeFileName . '.html';   // ./breakfast/FriedChicken.html
+  }
+  else if($recipeTimeOfDay=="Lunch"){
+    $recipeFileName     =   str_replace(' ', '', ucwords($recipeName));  // ./lunch/FriedChicken.html
+    $recipeFile         =   './lunch/' . $recipeFileName . '.html';
+  }
+  else if($recipeTimeOfDay=="Dinner"){
+    $recipeFileName     =   str_replace(' ', '', ucwords($recipeName));  // ./dinner/FriedChicken.html
+    $recipeFile         =   './dinner/' . $recipeFileName . '.html';
+  }
 
   // page template
   $strOut = '<!DOCTYPE html>' . 
@@ -20,8 +33,14 @@
 
 
   // write to html file
-  $f = fopen($recipeFileName . ".html", "w");
+  
+  $f = fopen($recipeFile, "w");
   fwrite($f, $strOut);
   fclose($f);
+
+  // redirect to new page
+  header("Location: " . $recipeFile);
+  exit();
+
 
 ?>
