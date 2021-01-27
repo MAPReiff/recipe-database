315d18d042c767302644bc604b3f1ce706a9c3a0
Mitchell Reiff <mapr323@gmail.com>
2021-01-27 00:52:29 -0500

    Recipe Page Generation
    Added a text box for recipe origin
    Added a recipe html template
    Added the above template to the recipe generation php document
    Updated css to wrap lines from text boxes
    Removed now unused bootstrap assets

--- recipeGen.php
+++ recipeGen.php
@@ -1,14 +1,15 @@
 <?php
   // vars
-  $recipeName           =   $_POST['recipeName'];                         // Fried Chicken
-  $recipeImage          =   './assets/img' . $_POST['recipeImage'];       // http://link.website/chicken.png
-  $recipeServings       =   $_POST['recipeServings'];                     // 12
-  $recipePrepTime       =   $_POST['recipePrepTime'];                     // 1h 20m
-  $recipeCookTime       =   $_POST['recipeCookTime'];                     // 1h 20m
-  $recipeIngredients    =   $_POST['recipeIngredients'];                  // Chicken, Buttermilk
-  $recipeInstructions   =   $_POST['recipeInstructions'];                 // Put chicken in stuff
-  $recipeEquipment      =   $_POST['recipeEquipment'];                    // Cast iron
-  $recipeTimeOfDay      =   $_POST['recipeTimeOfDay'];                    // Lunch
+  $recipeName           =   ucwords($_POST['recipeName']);                                                           // Fried Chicken
+  $recipeImage          =   $_POST['recipeImage'];                                                                   // http://link.website/chicken.png
+  $recipeServings       =   $_POST['recipeServings'];                                                                // 12
+  $recipePrepTime       =   $_POST['recipePrepTime'];                                                                // 1h 20m
+  $recipeCookTime       =   $_POST['recipeCookTime'];                                                                // 1h 20m
+  $recipeIngredients    =   '&#8226; ' . str_replace(PHP_EOL, '<br />&#8226; ', $_POST['recipeIngredients']);        // Chicken, Buttermilk
+  $recipeInstructions   =   '&#8226; ' . str_replace(PHP_EOL, '<br />&#8226; ', $_POST['recipeInstructions']);       // Put chicken in stuff
+  $recipeEquipment      =   '&#8226; ' . str_replace(PHP_EOL, '<br />&#8226; ', $_POST['recipeEquipment']);          // Cast iron
+  $recipeTimeOfDay      =   $_POST['recipeTimeOfDay'];                                                               // Lunch
+  $recipeOrigin         =   $_POST['recipeOrigin'];                                                                  // http://cooking.website/friedchicken
 
   if($recipeTimeOfDay=="Breakfast"){
     $recipeFileName     =   str_replace(' ', '', ucwords($recipeName));
@@ -25,11 +26,39 @@
 
   // page template
   $strOut = '<!DOCTYPE html>' . 
-            '<html>' . 
-              '<Body>' . 
-                'Name - ' . $recipeName . '' . 
-              '</body>' . 
-            '</html>';
+    '<html>' . 
+      '<head>' . 
+        '<link href="../simple.css" rel=stylesheet>' . 
+        '<title>' . $recipeName . '</title>' . 
+      '</head>' . 
+      '<body>' . 
+        '<div class="container" style="margin-bottom: 20px;">' . 
+          '<div class="row">' . 
+            '<div class="col-xs-6">' . 
+              '<h1>' . $recipeName . '</h1>' . 
+              '<img src="' . $recipeImage . '" width="600" height="500">' . 
+              '<br>' . 
+              '<h2>Serves: ' . $recipeServings . '</h2>' . 
+              '<h2>Prep Time: ' . $recipePrepTime . '</h2>' . 
+              '<h2>Cook Time: ' . $recipeCookTime . '</h2>' . 
+              '<dl>' . 
+                '<dt><h2>Ingredients:</h2>' . 
+                  '<dd>' . $recipeIngredients . '' . 
+              '</dl>' . 
+              '<dl>' . 
+                '<dt><h2>Equipment:</h2>' . 
+                  '<dd>' . $recipeEquipment . '' . 
+              '</dl>' . 
+              '<dl>' . 
+                '<dt><h2>Instructions:</h2>' . 
+                  '<dd>' . $recipeInstructions . '' . 
+              '</dl>' . 
+               '<h2><a href="' . $recipeOrigin . '">Original Web Page</a></h2>' .
+            '</div>' . 
+         ' </div>' . 
+        '</div>' . 
+      '</body>' . 
+    '</html>';
 
 
   // write to html file
@@ -42,5 +71,5 @@
   header("Location: " . $recipeFile);
   exit();
 
-
 ?>
+
